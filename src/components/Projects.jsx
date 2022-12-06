dc98f83903050a405085a9d2d80fa92ee86dbeb8
asima jamal <asima jama>
2022-12-06 20:50:29 +0500

    2nd comit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,14 +19,14 @@ const Projects = () => {
 
     <div className="container-fluid  wrapper resp">
     <div className="row">
-      <ProjectDeatils Ui={portfolio} title="Portfolio Website" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/" description="Created using HTML Css React Js & Bootstraps" iconDes="Source Code"/>
-      <ProjectDeatils Ui={News} title="News App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/" description="Created using HTML React Js & Bootstraps" iconDes="Source Code"/>
-      <ProjectDeatils Ui={Textutil} title="TextUtils App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/" description="Created using HTML React Js & Bootstraps" iconDes="Source Code"/>
-      <ProjectDeatils Ui={Youtube} title="Youtube Playlist App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/" description="Created using HTML & React Js" iconDes="Source Code"/>
+      <ProjectDeatils Ui={portfolio} title="Portfolio Website" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/myportfolio" description="Created using HTML Css React Js & Bootstrap" iconDes="Source Code"/>
+      <ProjectDeatils Ui={News} title="News App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/news-app" description="Created using HTML React Js & Bootstrap" iconDes="Source Code"/>
+      <ProjectDeatils Ui={Textutil} title="TextUtils App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/github/tree/master/src/components" description="Created using HTML React Js & Bootstrap" iconDes="Source Code"/>
+      <ProjectDeatils Ui={Youtube} title="Youtube Playlist App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/Youtube" description="Created using HTML & React Js" iconDes="Source Code"/>
       <ProjectDeatils Ui={Ui} title="Covid-19 Web-UI Design" icon={<FaBehance/>} link="https://www.behance.net/gallery/149769199/Website-UI-Design" description="Created using Figma" iconDes="See here"/>
       <ProjectDeatils Ui={Ui2} title="Hospital-Dashboard UI Design" icon={<FaBehance/>} link="https://www.behance.net/gallery/147205743/Dashboard-UI-Design" description="Created using Figma" iconDes="See here"/>
-      <ProjectDeatils Ui={Ecommerce} title="Ecommerce-Website" icon={<FaGithub/>} link="https://www.behance.net/gallery/147205743/Dashboard-UI-Design" description="Created using Figma" iconDes="See here"/>
-      <ProjectDeatils Ui={Movie} title="Movie-Search-App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/Search-Movie-Website-context-api" description="Created using React Js + HTML + CSS" iconDes="Source Code"/>
+      <ProjectDeatils Ui={Ecommerce} title="Ecommerce-Website" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/ecommerce" description="Created using HTML Bootstrap & React Js" iconDes="Source Code"/>
+      <ProjectDeatils Ui={Movie} title="Movie-Search-App" icon={<FaGithub/>} link="https://github.com/AsimaJamal789/context-api" description="Created using HTML Css & React Js" iconDes="Source Code"/>
       <ProjectDeatils Ui={wordpress} title="Ecommerce-Store" icon={<FaWordpress/>} link="http://wordpressshop.epizy.com/" description="Created using Wordpress" iconDes="See here"/>
       
     </div>
